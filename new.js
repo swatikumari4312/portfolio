013669559c8c7b816ebc6bd86e4809f6fa8c7032
swatikumari4312 <153591610+swatikumari4312@users.noreply.github.com>
2024-06-17 21:41:05 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,7 @@
+let typed= new Typed(".text",{
+    strings:[" Frontend developer " ,"Web Developer","Sports Player"],
+    typeSpeed: 10,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
+});
